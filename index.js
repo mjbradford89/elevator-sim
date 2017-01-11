4cdde7638b2cdc8dab808145699d9be5a4118c97
Michael Bradford <mbradford@veritone.com>
2017-01-11 13:36:22 -0800

    initial ElevatorCarOperator class

--- index.js
+++ index.js
@@ -4,22 +4,30 @@ const MIN_FLOOR = 1;
 const MAX_FLOOR = 10;
 
 class ElevatorCar extends EventEmitter {
-	constructor(floor) {
+	constructor(floor, carName) {
 		super();
 
+		if (floor < MIN_FLOOR || floor > MAX_FLOOR) {
+			floor = null;
+		}
+
+		this.name = carName;
 		this.floor = floor || 1;
 		this.open = false;
 		this.floorsPassed = 0;
+		this.tripsMade = 0;
 		this.moving = false;
 	}
 
 	//go to the desired floor
+	//returns a promise that is resolved when destination is reached
 	goToFloor(floor) {
 		var instance = this;
 		if (floor < MIN_FLOOR || floor > MAX_FLOOR) {
 			return;
 		}
 		instance.moving = true;
+		instance.direction = floor < instance.floor ? 'down' : 'up';
 		return new Promise(function(resolve, reject){
 			while (instance.floor != floor) {
 				if (instance.floor < floor) {
@@ -29,6 +37,11 @@ class ElevatorCar extends EventEmitter {
 				}
 			}
 			instance.moving = false;
+			instance.direction = null;
+			instance.tripsMade++;
+			instance.emit('tripMade', {
+				tripsMade: instance.tripsMade
+			});
 			resolve();
 		});
 	}
@@ -69,7 +82,41 @@ class ElevatorCar extends EventEmitter {
 	}
 }
 
-var carA = new ElevatorCar();
+class ElevatorCarOperator {
+	constructor() {
+		this.cars = [new ElevatorCar(1, 'Andy'), new ElevatorCar(5, 'Woody'), new ElevatorCar(10, 'Buzz')];
+	}
+
+	callCar(currentFloor) {
+		var shortestDistance = MAX_FLOOR,
+			targetCar = this.cars[0];
+
+		for (car in cars) {
+			if ((currentFloor < car.floor && car.direction === 'down') ||
+				(currentFloor > car.floor && car.direction === 'up') ||
+				(car.floor === currentFloor)) {
+
+				targetCar = car;
+				break;
+			}
+			var distanceToFloor = car.distanceToFloor(currentFloor);
+			if (distanceToFloor < shortestDistance) {
+				shortestDistance = car.distanceToFloor(currentFloor);
+				targetCar = car;
+			}
+		}
+
+		if (targetCar.moving) {
+			console.log(targetCar.name + " is on its' way to get you!");
+		} else {
+			return targetCar.goToFloor(currentFloor).then(function() {
+				console.log(targetCar.name + " is here to pick you up!");
+			});
+		}
+
+	}
+}
+
 
 carA.on('floorChanged', function(event) {
 	console.log('floorChanged:' + event.newFloor);
